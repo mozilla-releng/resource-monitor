307c2ffe21273d0b240d9f118981d84bfb19c4fe
Simon Fraser <sfraser@mozilla.com>
2020-07-01 14:49:20 +0100

    Trim output format

--- main.go
+++ main.go
@@ -15,41 +15,66 @@ import (
 
 	"github.com/shirou/gopsutil/cpu"
 	"github.com/shirou/gopsutil/mem"
-	"github.com/shirou/gopsutil/net"
 	"github.com/shirou/gopsutil/process"
 )
 
+// CPUTimesStat is a more limited version of cpu.TimesStat to save storage space
+type CPUTimesStat struct {
+	User   float64 `json:"user"`
+	System float64 `json:"system"`
+	Idle   float64 `json:"idle"`
+	Iowait float64 `json:"iowait"`
+	Steal  float64 `json:"steal"`
+}
+
+// ProcMemoryInfoStat is a more limited version of process.MemoryInfoStat
+type ProcMemoryInfoStat struct {
+	RSS  uint64 `json:"rss"`  // bytes
+	VMS  uint64 `json:"vms"`  // bytes
+	Swap uint64 `json:"swap"` // bytes
+}
+
+// ProcNetworkIOStat is a more limited version of net.IOCountersStat
+type ProcNetworkIOStat struct {
+	BytesSent   uint64 `json:"bytes_sent"`   // number of bytes sent
+	BytesRecv   uint64 `json:"bytes_recv"`   // number of bytes received
+	PacketsSent uint64 `json:"packets_sent"` // number of packets sent
+	PacketsRecv uint64 `json:"packets_recv"` // number of packets received
+}
+
+// ProcDiskIOStat exists to override the json field names of proc.IOCountersStat
+type ProcDiskIOStat struct {
+	ReadCount  uint64 `json:"read_count"`
+	WriteCount uint64 `json:"write_count"`
+	ReadBytes  uint64 `json:"read_bytes"`
+	WriteBytes uint64 `json:"write_bytes"`
+}
+
 // MozProcessStat combines existing structs into one.
 type MozProcessStat struct {
-	Timestamp int64                  `json:"timestamp"`
-	Memory    process.MemoryInfoStat `json:"memory"`  // all uint64
-	CPU       cpu.TimesStat          `json:"cpu"`     // all float64 apart from CPU: string
-	DiskIO    process.IOCountersStat `json:"disk"`    // all uint64
-	NetworkIO net.IOCountersStat     `json:"network"` // all uint64
+	Timestamp       int64              `json:"timestamp"`
+	Memory          ProcMemoryInfoStat `json:"memory"`  // all uint64
+	CPU             CPUTimesStat       `json:"cpu"`     // all float64
+	DiskIO          ProcDiskIOStat     `json:"disk"`    // all uint64
+	NetworkIO       ProcNetworkIOStat  `json:"network"` // all uint64
+	AvailableMemory uint64             `json:"available_memory"`
+	// AvailableMemory is kept separate because it's System-wide and must not be summed
 }
+
+// ignore network: fifoin fifoout?
 
 // Add the provided MozProcessStat to the current one.
 func (m *MozProcessStat) Add(data MozProcessStat) {
 	// Maybe there's a way of doing this with reflect
 	m.Memory.RSS += data.Memory.RSS
 	m.Memory.VMS += data.Memory.VMS
-	m.Memory.HWM += data.Memory.HWM
-	m.Memory.Data += data.Memory.Data
-	m.Memory.Stack += data.Memory.Stack
-	m.Memory.Locked += data.Memory.Locked
 	m.Memory.Swap += data.Memory.Swap
 
-	// Ignore CPU Name string
 	m.CPU.User += data.CPU.User
 	m.CPU.System += data.CPU.System
 	m.CPU.Idle += data.CPU.Idle
-	// Ignore CPU Nice, as that doesn't meaningfully sum
 	m.CPU.Iowait += data.CPU.Iowait
-	m.CPU.Irq += data.CPU.Irq
-	m.CPU.Softirq += data.CPU.Softirq
 	m.CPU.Steal += data.CPU.Steal
-	m.CPU.Guest += data.CPU.Guest
-	// Ignore CPU GuestNice, as that doesn't meaningfully sum
 
 	m.DiskIO.ReadCount += data.DiskIO.ReadCount
 	m.DiskIO.WriteCount += data.DiskIO.WriteCount
@@ -60,25 +85,17 @@ func (m *MozProcessStat) Add(data MozProcessStat) {
 	m.NetworkIO.BytesRecv += data.NetworkIO.BytesRecv
 	m.NetworkIO.PacketsSent += data.NetworkIO.PacketsSent
 	m.NetworkIO.PacketsRecv += data.NetworkIO.PacketsRecv
-	m.NetworkIO.Errin += data.NetworkIO.Errin
-	m.NetworkIO.Errout += data.NetworkIO.Errout
-	m.NetworkIO.Dropin += data.NetworkIO.Dropin
-	m.NetworkIO.Dropout += data.NetworkIO.Dropout
-	m.NetworkIO.Fifoin += data.NetworkIO.Fifoin
-	m.NetworkIO.Fifoout += data.NetworkIO.Fifoout
 }
 
 // Diff the provided MozProcessStat to the current one.
 func (m *MozProcessStat) Diff(data MozProcessStat) {
-	// Ignore CPU string
+	// Memory fields are absolute, not a sum, so don't diff those.
+
 	m.CPU.User -= data.CPU.User
 	m.CPU.System -= data.CPU.System
 	m.CPU.Idle -= data.CPU.Idle
 	m.CPU.Iowait -= data.CPU.Iowait
-	m.CPU.Irq -= data.CPU.Irq
-	m.CPU.Softirq -= data.CPU.Softirq
 	m.CPU.Steal -= data.CPU.Steal
-	m.CPU.Guest -= data.CPU.Guest
 
 	m.DiskIO.ReadCount -= data.DiskIO.ReadCount
 	m.DiskIO.WriteCount -= data.DiskIO.WriteCount
@@ -89,18 +106,12 @@ func (m *MozProcessStat) Diff(data MozProcessStat) {
 	m.NetworkIO.BytesRecv -= data.NetworkIO.BytesRecv
 	m.NetworkIO.PacketsSent -= data.NetworkIO.PacketsSent
 	m.NetworkIO.PacketsRecv -= data.NetworkIO.PacketsRecv
-	m.NetworkIO.Errin -= data.NetworkIO.Errin
-	m.NetworkIO.Errout -= data.NetworkIO.Errout
-	m.NetworkIO.Dropin -= data.NetworkIO.Dropin
-	m.NetworkIO.Dropout -= data.NetworkIO.Dropout
-	m.NetworkIO.Fifoin -= data.NetworkIO.Fifoin
-	m.NetworkIO.Fifoout -= data.NetworkIO.Fifoout
 }
 
 // SystemMemoryInfo summarises information about the system memory usage
 type SystemMemoryInfo struct {
-	TotalMemory      uint64  `json:"vmem_total`
-	TotalSwap        uint64  `json:"swap_total"`
+	TotalMemory uint64 `json:"vmem_total"`
+	TotalSwap   uint64 `json:"swap_total"`
 }
 
 // SystemInfo summarises information about the instance
@@ -142,7 +153,8 @@ func collectStatsForWithError(proc *process.Process, withError bool) (*MozProces
 			log.Printf("CPU Times: %s\n", err)
 		}
 	} else {
-		statistics.CPU = *cpu
+		statistics.CPU = CPUTimesStat{cpu.User, cpu.System, cpu.Idle, cpu.Iowait, cpu.Steal}
+		// statistics.CPU = *cpu
 	}
 
 	memory, err := proc.MemoryInfo()
@@ -151,7 +163,7 @@ func collectStatsForWithError(proc *process.Process, withError bool) (*MozProces
 			log.Printf("MemoryInfo: %s\n", err)
 		}
 	} else {
-		statistics.Memory = *memory
+		statistics.Memory = ProcMemoryInfoStat{memory.RSS, memory.VMS, memory.Swap}
 	}
 
 	diskio, err := proc.IOCounters()
@@ -160,10 +172,10 @@ func collectStatsForWithError(proc *process.Process, withError bool) (*MozProces
 			log.Printf("Disk IO: %s\n", err)
 		}
 	} else {
-		statistics.DiskIO = *diskio
+		statistics.DiskIO = ProcDiskIOStat{diskio.ReadCount, diskio.WriteCount, diskio.ReadBytes, diskio.WriteBytes}
 	}
 
-	total := new(net.IOCountersStat)
+	total := new(ProcNetworkIOStat)
 	netio, err := proc.NetIOCounters(false)
 	if err != nil {
 		if withError {
@@ -175,12 +187,6 @@ func collectStatsForWithError(proc *process.Process, withError bool) (*MozProces
 			total.BytesRecv += iface.BytesRecv
 			total.PacketsSent += iface.PacketsSent
 			total.PacketsRecv += iface.PacketsRecv
-			total.Errin += iface.Errin
-			total.Errout += iface.Errout
-			total.Dropin += iface.Dropin
-			total.Dropout += iface.Dropout
-			total.Fifoin += iface.Fifoin
-			total.Fifoout += iface.Fifoout
 		}
 		statistics.NetworkIO = *total
 	}
@@ -209,6 +215,13 @@ func collector(fh *os.File) {
 		statistics.Add(*procstats)
 	}
 
+	memory, err := mem.VirtualMemory()
+	if err != nil {
+		log.Printf("Unable to collect system memory statistics\n")
+		return
+	}
+	statistics.AvailableMemory = memory.Available
+
 	jsonData, err := json.Marshal(statistics)
 	if err != nil {
 		fmt.Printf("Couldn't format data as json: %v", err)
@@ -223,20 +236,20 @@ func collector(fh *os.File) {
 
 func getSystemInfo() *SystemInfo {
 	info := new(SystemInfo)
-	mem_info := new(SystemMemoryInfo)
+	memInfo := new(SystemMemoryInfo)
 
 	memory, err := mem.VirtualMemory()
 	if err != nil {
 		log.Fatal(err)
 	}
-	mem_info.TotalMemory = memory.Total
+	memInfo.TotalMemory = memory.Total
 
 	swap, err := mem.SwapMemory()
 	if err != nil {
 		log.Fatal(err)
 	}
-	mem_info.TotalSwap = swap.Total
-	info.MemoryStats = *mem_info
+	memInfo.TotalSwap = swap.Total
+	info.MemoryStats = *memInfo
 
 	cpuLogCount, err := cpu.Counts(true)
 	if err != nil {
